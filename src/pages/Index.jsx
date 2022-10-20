0c6354615dd7c92a4f62d416ec2726de95f6b3d2
centinela1024tb <centinela1024tb@gmail.com>
2022-10-20 14:49:42 -0500

    [ADD] Se agrega la tabla con el listado de los clientes
    => Para mostrar los clientes se agregará uncomponente con cada <tr><th> ..info  </th></tr>

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from 'react-router-dom'
+import Cliente from '../components/Cliente';
 
 export function loader() {
   /* 
@@ -48,13 +49,39 @@ const Index = () => {
 
   // podemos acceder al loader por medio del hook useLoaderData
   const clientes = useLoaderData();
-  console.log(clientes)
+  // console.log(clientes)
 
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3">Administra tus clientes</p>
+      <hr />
       
+      {clientes.length 
+        ? (
+          <table className="w-full bg-white shadow mt-5 table-auto">
+            <thead className="bg-blue-800 text-white">
+              <tr>
+                <th className="p-2">Cliente</th>
+                <th className="p-2">Contacto</th>
+                <th className="p-2">Acciones</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {clientes.map( cliente => (
+                <Cliente 
+                  key={cliente.id}
+                  cliente={cliente}
+                />
+              ))}
+            </tbody>
+
+          </table>
+        ) 
+        : (
+          <p className="text-center mt-10">No hay Clientes</p>    
+        )}
     </>
   )
 }
