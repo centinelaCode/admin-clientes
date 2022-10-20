db89753ee77971b5809ec454fce9e93dfb0319e1
centinela1024tb <centinela1024tb@gmail.com>
2022-10-20 17:20:01 -0500

    [ADD] Se finaliza la validación del formulario
    => Se valida que todos los campos son requeridos
    => Y que el email sea un correo valido, para eso se usa una expresión regular
    => Se renderizan en un nuevo componente Error.jsx para mostrar el mensaje

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = ({children}) => {
+  return (
+    <div className="text-center py-4 my-4 bg-red-600 text-white uppercase rounded-md" >
+      {children}
+    </div>
+  )
+}
+
+export default Error
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,7 @@
-import { useNavigate, Form } from 'react-router-dom'
+import { useNavigate, Form, useActionData } from 'react-router-dom'
+
 import Formulario from '../components/Formulario';
+import Error from '../components/Error';
 
 export async function action({request}) {
   // console.log(request);
@@ -16,6 +18,30 @@ export async function action({request}) {
   const data = Object.fromEntries(formDat);
   // console.log(data)
 
+  // obtenemos el email ingresado para validarlo
+  const email = formDat.get('email');
+
+  // array  de errores
+  const errors = [];
+
+  // Validación
+  // validamos que se inegresen todos los datos
+  if(Object.values(data).includes('')) {
+    errors.push('Todos los campos son obligatorios')
+  }
+
+  // validamos que sea un email valido
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)) {
+    errors.push('El email no es valido')
+  }
+  
+  
+  // retornar datos si hay error
+  if(Object.keys(errors).length) {
+    return errors;
+  }
+   
   
 }
 
@@ -24,6 +50,8 @@ const NuevoCliente = () => {
 
   // para redireccionar
   const navigate = useNavigate();
+  const errores = useActionData();
+  console.log(errores)
   
 
   return (
@@ -37,11 +65,17 @@ const NuevoCliente = () => {
           className="bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded-md uppercase font-bold "
           onClick={ () => navigate(-1)}
         >Volver</button>
-      </div>
+      </div>      
 
-      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-10">   
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-10 font-bold">   
+
+        {
+          errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )
+        }
+
         <Form
           method='POST'
+          noValidate
         >
             <Formulario />
 
