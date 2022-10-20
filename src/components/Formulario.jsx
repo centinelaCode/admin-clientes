6b17b0997b27c8cd64dd521dc18ed84b7bacc914
centinela1024tb <centinela1024tb@gmail.com>
2022-10-20 16:10:18 -0500

    [ADD] Se finaliza la maquetación de Nuevo Cliente

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,77 @@
+const Formulario = ({cliente}) => {
+  return (
+    <>
+      <div className="mb-4">
+        <label
+            className="text-gray-800 font-bold"
+            htmlFor="nombre"
+        >Nombre:</label>
+        <input 
+            id="nombre"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Cliente"
+            name="nombre"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+            className="text-gray-800 font-bold"
+            htmlFor="empresa"
+        >Empresa:</label>
+        <input 
+            id="empresa"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Empresa del Cliente"
+            name="empresa"
+        />
+      </div>
+
+      <div className="mb-4">
+        <label
+            className="text-gray-800 font-bold"
+            htmlFor="email"
+        >E-mail:</label>
+        <input 
+            id="email"
+            type="email"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Email del Cliente"
+            name="email"
+        />
+      </div>
+
+      <div className="mb-4">
+        <label
+            className="text-gray-800 font-bold"
+            htmlFor="telefono"
+        >Teléfono:</label>
+        <input 
+            id="telefono"
+            type="tel"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Teléfono del Cliente"
+            name="telefono"
+        />
+      </div>
+
+      <div className="mb-4">
+        <label
+            className="text-gray-800 font-bold"
+            htmlFor="notas"
+        >Notas:</label>
+        <textarea
+            as="textarea"
+            id="notas"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50 h-40 align-self"
+            placeholder="Notas del Cliente"
+            name="notas"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Formulario
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,37 @@
+import { useNavigate } from 'react-router-dom'
+import Formulario from '../components/Formulario';
+
 
 const NuevoCliente = () => {
+
+  // para redireccionar
+  const navigate = useNavigate();
+  
+
   return (
     <>
-      <h1>Nuevo Cliente</h1>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Clientes</h1>
+      <p className="mt-3">Completa la información que se solicita para registrar un nuevo cliente </p>
+      <hr />
+
+      <div className="flex justify-end mt-3">
+        <button
+          className="bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded-md uppercase font-bold "
+          onClick={ () => navigate(-1)}
+        >Volver</button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx auto px-5 py-10 mt-10">   
+        <form>
+            <Formulario />
+
+            <input 
+              type="submit"
+              className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+              value="Registrar Cliente"
+            />          
+        </form>     
+      </div>
     </>
   )
 }
