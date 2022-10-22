986d41f48487ea044249592adb4552233b602a0c
centinela1024tb <centinela1024tb@gmail.com>
2022-10-22 13:53:19 -0500

    [ADD] Se hace la implementación del formulario
    => Se importan de reac-router-dom: useNaviagate y Form
    => Se impelementa el formulario y se mustra el cliente a editar
    => Para mostrar la info en los inputs se usa defaultValue ya que si se hace con value solicita en onChange

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, Form, useNavigate } from 'react-router-dom'
 import { getCliente } from "../data/clientes";
+import Formulario from '../components/Formulario';
 
 export async function loader({params}) {
   const clienteId = params.clienteId;
@@ -19,14 +20,47 @@ export async function loader({params}) {
 
 
 const EditarCliente = () => {
+  // para obtner el navigate del hook useNaviagte
+  const navigate = useNavigate();
 
-  // podemos acceder al loader por medio del hook useLoaderData
+  // para acceder al loader por medio del hook useLoaderData  
   const cliente = useLoaderData();
   
 
   return (
     <>
-      <h1>{cliente.id} - {cliente.nombre}</h1>
+      <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
+      <p className="mt-3">A continuación podrás modificar los datos de un cliente</p>
+      <hr />
+
+      <div className="flex justify-end mt-3">
+        <button
+          className="bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded-md uppercase font-bold "
+          onClick={ () => navigate(-1)}
+        >Volver</button>
+      </div>      
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-10 font-bold">   
+
+        {/* {
+          errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )
+        } */}
+
+        <Form
+          method='POST'
+          noValidate
+        >
+            <Formulario 
+              cliente={cliente}
+            />
+
+            <input 
+              type="submit"
+              className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+              value="Registrar Cliente"
+            />          
+        </Form>     
+      </div>
     </>
   )
 }
