6b3cadc61ecc8d9fb3905185daf66eeba7faab62
centinela1024tb <centinela1024tb@gmail.com>
2022-10-22 13:24:19 -0500

    [ADD] Se agrega el metodo getCliente y le pasamos el id del cliente a obtener
    => A la url que teniamos en las variables de entorno le agregamos el id

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -7,6 +7,15 @@ export async function getClientes() {
 }
 
 
+export async function getCliente(id) {
+  const url = `${import.meta.env.VITE_API_URL}/${id}`;
+
+  const respuesta = await fetch(url)
+  const resultado = await respuesta.json();  
+  return resultado;
+}
+
+
 export async function addCliente(data) {
   try {
     const url = import.meta.env.VITE_API_URL;
